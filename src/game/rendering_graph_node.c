a8b104607921ae45add11183fcb41f493427e4a3
Johnny on Flame <johnnyonflame@hotmail.com>
2020-10-15 14:11:44 -0300

    Restore LODs for OpenDingux targets.

--- src/game/rendering_graph_node.c
+++ src/game/rendering_graph_node.c
@@ -277,7 +277,7 @@ static void geo_process_level_of_detail(struct GraphNodeLevelOfDetail *node) {
     s16 distanceFromCam = -GET_HIGH_S16_OF_32(mtx->m[1][3]); // z-component of the translation column
 #endif
 
-#ifndef TARGET_N64
+#if !defined(TARGET_N64) && !defined(TARGET_OD)
     // We assume modern hardware is powerful enough to draw the most detailed variant
     distanceFromCam = 0;
 #endif
