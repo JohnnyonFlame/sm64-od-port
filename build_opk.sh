499b63b6fb0cb896eb86d6c450386e541b265ed7
Johnny on Flame <johnnyonflame@hotmail.com>
2020-11-06 05:10:08 -0300

    build_opk.sh: Use -noappend flag. This fixes mksquashfs appending data already existing OPK files instead of overwriting.

--- build_opk.sh
+++ build_opk.sh
@@ -5,4 +5,4 @@ convert textures/segment2/segment2.05A00.rgba16.png -resize 32x32! build/icon.pn
 mksquashfs \
     build/us_pc/sm64.us.f3dex2e         \
     build/icon.png                      \
-    default.gcw0.desktop sm64-port.opk
+    default.gcw0.desktop sm64-port.opk -noappend
